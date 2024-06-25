562d66e60c5acc08ec5406ca5cad815b841c95dd
meomun1 <maluong1504@gmail.com>
2024-06-25 22:19:01 +0700

    update page

---
+++ src/app/(main)/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+
+export default function Home() {
+	return (
+		<>
+            <div>
+                <h1>Home</h1>
+                <p>Welcome to the home page</p>
+            </div>
+		</>
+	)
+}
